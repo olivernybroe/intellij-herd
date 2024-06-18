cf230e44041c60c0c9cf004779309d62f3764488
GitHub Action <action@github.com>
2024-06-18 09:18:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/olivernybroe/intellijherd/MyBundle.kt
+++ src/main/kotlin/com/github/olivernybroe/intellijherd/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.olivernybroe.intellijherd
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/olivernybroe/intellijherd/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/olivernybroe/intellijherd/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.olivernybroe.intellijherd.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
